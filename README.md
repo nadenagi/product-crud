b9e6a5f86bc9023e8eeb64a830899f06d26ec612
Nader Nagi <nadernagi2008@gmail.com>
2022-06-24 17:51:01 +0200

    updated validation and readme

--- README.md
+++ README.md
@@ -22,6 +22,16 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
 
+## Running prettier
+
+Run `npm run prettifyAll` to prettify all files
+
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Important notes
+
+- I used validation on the input [required, minLength, maxLength] to save http request from 431, 413, 414, 429 http errors
+- I used pattern validation on the input to make sure that the inputs doesn't contain special characters because some characters would be used in the sql queries
+- button should have had a validation as an html attripute and js validation
--- src/app/pages/products/product-form/product-form.component.ts
+++ src/app/pages/products/product-form/product-form.component.ts
@@ -1,101 +1,108 @@
 import {
-  Component,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  EventEmitter,
-} from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+    Component,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    EventEmitter,
+} from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.scss'],
+    selector: 'app-product-form',
+    templateUrl: './product-form.component.html',
+    styleUrls: ['./product-form.component.scss'],
 })
 export class ProductFormComponent implements OnInit, OnChanges {
-  @Input() selectedProduct: any;
-  @Output() closeForm = new EventEmitter();
-  @Output() addEditProduct = new EventEmitter();
-  @Output() deleteProductEmit = new EventEmitter();
-  productForm: FormGroup = new FormGroup({});
-  showenPasswrd: boolean = false;
-  submitted: boolean = false;
-  productTypes: any[] = [
-    {
-      name: 'Type 1',
-      value: 1,
-    },
-    {
-      name: 'Type 2',
-      value: 2,
-    },
-  ];
+    @Input() selectedProduct: any
+    @Output() closeForm = new EventEmitter()
+    @Output() addEditProduct = new EventEmitter()
+    @Output() deleteProductEmit = new EventEmitter()
+    productForm: FormGroup = new FormGroup({})
+    showenPasswrd: boolean = false
+    submitted: boolean = false
+    productTypes: any[] = [
+        {
+            name: 'Type 1',
+            value: 1,
+        },
+        {
+            name: 'Type 2',
+            value: 2,
+        },
+    ]
 
-  constructor() {
-    this.setForm();
-  }
-
-  setForm() {
-    this.productForm = new FormGroup({
-      id: new FormControl(this.selectedProduct?.id),
-      name: new FormControl(this.selectedProduct?.name, Validators.required),
-      productType: new FormControl(
-        this.selectedProduct?.productType,
-        Validators.required
-      ),
-      productCategory: new FormControl(
-        this.selectedProduct?.productCategory,
-        Validators.required
-      ),
-      subCategory: new FormControl(this.selectedProduct?.subCategory),
-      referenceId: new FormControl(this.selectedProduct?.referenceId),
-      password: new FormControl(
-        this.selectedProduct?.password,
-        Validators.required
-      ),
-      deleveryFeesAmount: new FormControl(
-        this.selectedProduct?.deleveryFeesAmount
-      ),
-      deleveryFeesPercentage: new FormControl(
-        this.selectedProduct?.deleveryFeesPercentage
-      ),
-    });
-  }
-
-  ngOnChanges(): void {
-    this.setForm();
-    this.submitted = false;
-  }
-
-  ngOnInit(): void {
-    this.submitted = false;
-  }
-
-  saveProduct() {
-    if (this.productForm.valid) {
-      this.addEditProduct.emit(this.productForm.value);
-    } else {
-      this.submitted = true;
+    constructor() {
+        this.setForm()
     }
-  }
 
-  deleteProduct() {
-    this.deleteProductEmit.emit(this.productForm.value);
-    this.submitted = false;
-  }
-
-  cancelEdit() {
-    this.closeForm.emit();
-    this.submitted = false;
-  }
-
-  chanegePasswordType() {
-    let x = document.getElementById('inputPassword') as HTMLInputElement;
-    if (x.type === 'password') {
-      x.type = 'text';
-    } else {
-      x.type = 'password';
+    setForm() {
+        this.productForm = new FormGroup({
+            id: new FormControl(this.selectedProduct?.id),
+            name: new FormControl(this.selectedProduct?.name, [
+                Validators.required,
+                Validators.minLength(5),
+                Validators.maxLength(20),
+                Validators.pattern('^[a-zA-Z0-9]{5,}$'),
+            ]),
+            productType: new FormControl(this.selectedProduct?.productType, [
+                Validators.required,
+                Validators.minLength(5),
+                Validators.maxLength(20),
+                Validators.pattern('^[a-zA-Z0-9]{5,}$'),
+            ]),
+            productCategory: new FormControl(
+                this.selectedProduct?.productCategory,
+                [
+                    Validators.required,
+                    Validators.minLength(5),
+                    Validators.maxLength(20),
+                    Validators.pattern('^[a-zA-Z0-9]{5,}$'),
+                ]
+            ),
+            subCategory: new FormControl(this.selectedProduct?.subCategory),
+            referenceId: new FormControl(this.selectedProduct?.referenceId),
+            password: new FormControl(this.selectedProduct?.password, [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(15),
+            ]),
+            deleveryFeesAmount: new FormControl(
+                this.selectedProduct?.deleveryFeesAmount
+            ),
+            deleveryFeesPercentage: new FormControl(
+                this.selectedProduct?.deleveryFeesPercentage
+            ),
+        })
     }
-  }
+
+    ngOnChanges(): void {
+        this.setForm()
+        this.submitted = false
+    }
+
+    ngOnInit(): void {
+        this.submitted = false
+    }
+
+    saveProduct() {
+        this.productForm.valid
+            ? this.addEditProduct.emit(this.productForm.value)
+            : (this.submitted = true)
+    }
+
+    deleteProduct() {
+        this.deleteProductEmit.emit(this.productForm.value)
+        this.submitted = false
+    }
+
+    cancelEdit() {
+        this.closeForm.emit()
+        this.submitted = false
+    }
+
+    chanegePasswordType() {
+        let x = document.getElementById('inputPassword') as HTMLInputElement
+        x.type = x.type === 'password' ? 'text' : 'password'
+    }
 }
